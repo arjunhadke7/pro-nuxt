e4282f086b429846274a0120fa3a167829ca197a
Arjun Hadke <arjun.swcm@gmail.com>
2023-01-21 08:07:11 +0530

    code so far

---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit magni recusandae perferendis id quae deserunt, magnam praesentium in sit fuga, aspernatur tenetur quibusdam nesciunt nulla unde aliquid alias vitae labore porro esse aut laborum optio pariatur at. Quia, mollitia dolore?</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style >
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container mx-auto py-2">
+        <div class="flex flex-wrap">
+        <div class="container mx-auto md:w-3/4">
+            <h1 class="text-4xl mb-2">Home page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit vel ab veritatis, itaque eveniet explicabo numquam saepe distinctio, quam eius rerum expedita quod voluptatibus earum temporibus consectetur maxime vitae aperiam.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit vel ab veritatis, itaque eveniet explicabo numquam saepe distinctio, quam eius rerum expedita quod voluptatibus earum temporibus consectetur maxime vitae aperiam.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit vel ab veritatis, itaque eveniet explicabo numquam saepe distinctio, quam eius rerum expedita quod voluptatibus earum temporibus consectetur maxime vitae aperiam.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit vel ab veritatis, itaque eveniet explicabo numquam saepe distinctio, quam eius rerum expedita quod voluptatibus earum temporibus consectetur maxime vitae aperiam.</p>
+        </div>
+        <div class="container mx-auto sidebar md:w-1/4">
+            <h1 class="text-3xl mb-2">Sidebar</h1>
+            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, doloribus cum. Unde et corporis ipsam accusantium odit quos, maiores enim!</p>
+            <p> Lorem ipsum dolor sit  consectetur, adipisicing elit. Odio, doloribus cum. Unde et corporis ipsam accusantium odit quos, maiores enim!</p>
+            <h2 class="text-3xl mb-2">enterprises</h2>
+            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, doloribus cum. Unde et corporis ipsam accusantium odit quos, maiores enim!</p>
+            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, doloribus cum. Unde et corporis ipsam accusantium odit quos, maiores enim!</p>
+            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, doloribus cum. Unde et corporis ipsam accusantium odit quos, maiores enim!</p>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
