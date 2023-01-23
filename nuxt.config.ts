9ab988c536e3d5f072cc2268804ef54dc3c21af6
Arjun Hadke <arjun.swcm@gmail.com>
2023-01-24 04:11:54 +0530

    atx removed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
-
+import atx from "./atx"
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -6,13 +6,7 @@ export default defineNuxtConfig({
     app: {
         head: {
             script: [
-                {
-                    // children: "https://umami.atxsite.space/umami.js",
-                    async: true,
-                    defer: true,
-                    src: "https://umami.atxsite.space/umami.js",
-                    'data-website-id': 'c52f0aab-f414-4fe6-b518-3389ec3b8562'
-                }
+                atx
             ]
         }
         
